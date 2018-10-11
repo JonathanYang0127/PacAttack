93181a59d135d30cc288ffd4a267c019c6f6b5c5
JonathanYang0127 <jydumplings@gmail.com>
2018-10-10 19:26:08 -0700

    Started creating initial layout

--- game.js
+++ game.js
@@ -1,5 +1,25 @@
+var rows = 20, columns = 20, buffer = 3;
+var gridwidth, gridheight;
+
 var create_game = function(){
-    alert("Hello, World!")
+    try{
+        var canvas = document.getElementById("gameCanvas");
+        var c2d = canvas.getContext("2d");
+    }
+    catch{
+        alert("Game Canvas Not Found");
+    }
+    gridwidth = Math.floor(canvas.width/columns); 
+    gridheight = Math.floor(canvas.height/rows);
+    canvas.width = gridwidth*columns+buffer;
+    canvas.height = gridheight*rows+buffer;
+    c2d.lineWidth = buffer;
+    c2d.beginPath();
+    for(var i = 0; i<=columns; i++){
+        c2d.moveTo(gridwidth*i+2, 0);
+        c2d.lineTo(gridwidth*i+2, canvas.height);
+        c2d.stroke();
+    }
 };
 
-create_game()
+create_game();
