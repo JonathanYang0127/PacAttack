735ccfc3a0af36b78775b271055a69ecc4ac161f
JonathanYang0127 <jydumplings@gmail.com>
2018-10-11 12:20:44 -0700

    Added grid which changes width depending on the number of rows/columns and the size of the canvas

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var rows = 20, columns = 20, buffer = 3;
+var rows = 15, columns = 15, buffer = 3;
 var gridwidth, gridheight;
 
 var create_game = function(){
@@ -11,13 +11,19 @@ var create_game = function(){
     }
     gridwidth = Math.floor(canvas.width/columns); 
     gridheight = Math.floor(canvas.height/rows);
-    canvas.width = gridwidth*columns+buffer;
-    canvas.height = gridheight*rows+buffer;
+    canvas.width = gridwidth*columns-buffer;
+    canvas.height = gridheight*rows-buffer;
+    canvas.style = style="border:3px solid #000000;"
     c2d.lineWidth = buffer;
     c2d.beginPath();
-    for(var i = 0; i<=columns; i++){
-        c2d.moveTo(gridwidth*i+2, 0);
-        c2d.lineTo(gridwidth*i+2, canvas.height);
+    for(var i = 1; i<columns; i++){
+        c2d.moveTo(gridwidth*i-buffer, 0);
+        c2d.lineTo(gridwidth*i-buffer, canvas.height);
+        c2d.stroke();
+    }
+    for(var i = 1; i<rows; i++){
+        c2d.moveTo(0, gridwidth*i-buffer);
+        c2d.lineTo(canvas.width, gridwidth*i-buffer, canvas);
         c2d.stroke();
     }
 };
