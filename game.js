590650544540d5936adf26d9e1039af16917bff6
JonathanYang0127 <jydumplings@gmail.com>
2018-10-13 19:14:50 -0700

    fixed many movement bugs

--- game.js
+++ game.js
@@ -11,30 +11,59 @@ var initialize = function(){
     draw_players();
 }
 
-var set_up_keys = function(){  
+var dir_to_x = function(dir){
+    return dir == 3 ? -1 : dir == 1 ? 1 : 0;
+}
+
+var dir_to_y = function(dir){
+    return dir == 0 ? -1 : dir == 2 ? 1 : 0;
+}
+
+var set_up_keys = function(){
     document.onkeydown = function(e) {
         var key = e.keyCode ? e.keyCode : e.which;
-        if(key == 37){
-            players[0].dir = !is_wall(players[0].x, players[0].y, 3) ? 3 : players[0].dir;
+        var nx0 = players[0].x+dir_to_x(players[0].dir);
+        var ny0 = players[0].y+dir_to_y(players[0].dir);
+        var nx1 = players[1].x+dir_to_x(players[1].dir);
+        var ny1 = players[1].x+dir_to_y(players[1].dir);
+        if(key == 37 && (!is_wall(nx0, ny0, 3) || players[0].blocked)){
+            players[0].rdir = 3;
         }
-        else if(key == 38) {
-            players[0].dir = !is_wall(players[0].x, players[0].y, 0) ? 0 : players[0].dir;
+        else if(key == 38 && (!is_wall(nx0, ny0, 0) || players[0].blocked)){
+            players[0].rdir = 0;
         }
-        else if(key == 39){
-            players[0].dir = !is_wall(players[0].x, players[0].y, 1) ? 1 : players[0].dir;
+        else if(key == 39 && (!is_wall(nx0, ny0, 1) || players[0].blocked)){
+            players[0].rdir = 1;
         }
-        else if(key == 40) {
-            players[0].dir = !is_wall(players[0].x, players[0].y, 2) ? 2 : players[0].dir;
+        else if(key == 40 && (!is_wall(nx0, ny0, 2) || players[0].blocked)) {
+            players[0].rdir = 2;
+        }
+        else if(key == 65 && (!is_wall(nx1, ny1, 3) || players[1].blocked)){
+            players[1].rdir = 3;
+        }
+        else if(key == 87 && (!is_wall(nx1, ny1, 0) || players[1].blocked)){
+            players[1].rdir = 0;
+        }
+        else if(key == 68 && (!is_wall(nx1, ny1, 1) || players[1].blocked)){
+            players[1].rdir = 1;
+        }
+        else if(key == 83 && (!is_wall(nx1, ny1, 2) || players[1].blocked)) {
+            players[1].rdir = 2;
         }
     }
 }
 
 
 var step = function(){
-    for(var speed = 0; speed<4; speed++){
-        for (var i = 0; i<players.length; i++){
+    for (var i = 0; i<players.length; i++){
+        for(var speed = 0; speed<8; speed++){
             if(!is_wall(players[i].x, players[i].y, players[i].dir)){
+                players[i].blocked = false;
                 players[i].move();
+            }
+            else{
+                players[i].blocked = true;
+                players[i].dir = players[i].rdir;
             }
         }
     }
--- game_objects.js
+++ game_objects.js
@@ -15,11 +15,13 @@ class Player{
         this.y = y;
         this.xreal = xreal;
         this.yreal = yreal;
+        this.blocked = true;
         this.dir = dir; //0 = up, 1 = right, 2 = down, 3 = left
+        this.rdir = this.dir
     }
 
     update_grid_values(){
-        var org = this.y
+        var orgx = this.x, orgy = this.y;
         var gridwidth = canvas.width/this.xnum;
         var gridheight = canvas.height/this.ynum;
         if(this.dir == 0){
@@ -34,14 +36,17 @@ class Player{
         else{
             this.x = Math.floor((this.xreal-gridwidth/2)/gridwidth)+1;
         }
+        if(!(this.x == orgx && orgy == this.y) || Math.abs(this.dir-this.rdir)%2 == 0){
+            this.dir = this.rdir;
+        }
     }
-    
-    move(){ 
+
+    move(){
         if(this.dir == 0){
             this.yreal--;
         }
         else if (this.dir == 1){
-            this.xreal++;   
+            this.xreal++;
         }
         else if (this.dir == 2){
             this.yreal++;
@@ -50,5 +55,5 @@ class Player{
            this.xreal--;
         }
         this.update_grid_values();
-    } 
+    }
 }
